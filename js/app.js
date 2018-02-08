3cf12e30a4b52005d4d05bc86b7ee52b9f284b40
Geekparade <david_da_silva0hotmail.fr>
2018-02-08 15:31:03 +0100

    mise en place de la nav bar

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // Test link JS
 	console.log("Hello !")
+
+// Function nav bar menu one for appear different nav bar menu two, when click a button in nav bar menu one
+// if no click a button in nav bar menu one, don't appear button menu two
+	var menuTwo = $(".menu-two").hide();
+ 	function navBarMenu(){
+
+
+	}
